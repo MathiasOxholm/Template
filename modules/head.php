b7acef0c6fc8c515dfe80abe94cfbf12a9b0ba17
Mathias Oxholm <mo.micheelsen@gmail.com>
2017-10-14 01:52:19 +0200

    php require head

---
+++ modules/head.php
@@ -0,0 +1,5 @@
+  <meta charset="utf-8">
+  <title>Sass template</title>
+  <link href="https://fonts.googleapis.com/css?family=Roboto:400,700" rel="stylesheet">
+  <link rel="stylesheet" href="styles/main.css">
+  <link rel="shortcut icon" href="images/sass.png">
--- work.php
+++ work.php
@@ -2,11 +2,7 @@
 <html>
 
 <head>
-  <meta charset="utf-8">
-  <title>Sass template</title>
-  <link href="https://fonts.googleapis.com/css?family=Roboto:400,700" rel="stylesheet">
-  <link rel="stylesheet" href="styles/main.css">
-  <link rel="shortcut icon" href="images/sass.png">
+  <?php require 'modules/head.php' ?>
 </head>
 
 <body>
